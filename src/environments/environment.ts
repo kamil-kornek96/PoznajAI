e48be690a08aaf4e2f9165bccd1135a554071bb9
Kamil Kornek <kamil.kornek96@gmail.com>
2024-01-25 14:53:09 +0100

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://poznajai-api.azurewebsites.net/',
+  apiUrl: 'https://poznajai-api.azurewebsites.net/api',
   url: 'https://poznajai-api.azurewebsites.net',
   openApiKey: '123'
 };
